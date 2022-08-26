ee672eb8750d8845ba7a53caeefae91def0ee3f9
pedro <pedroeugeniocavalcanti@gmail.com>
2022-08-26 15:13:03 -0300

    Implementação do metodo de getPayments no PaymentService do hr-payroll

--- hr-payroll/src/main/java/me/pedroeugenio/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/me/pedroeugenio/hrpayroll/services/PaymentService.java
@@ -1,12 +1,34 @@
 package me.pedroeugenio.hrpayroll.services;
 
 import me.pedroeugenio.hrpayroll.entities.Payment;
+import me.pedroeugenio.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayments(Long workerId, int days){
-        return new Payment("nome", 100.4, 10);
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayments(Long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+        Optional<Worker> workerOptional = Optional.ofNullable(restTemplate.getForObject(workerHost + "/workers/{id}",
+                Worker.class, uriVariables));
+        if (workerOptional.isPresent())
+            return new Payment(workerOptional.get().getName(), workerOptional.get().getDailyIncome(), 10);
+        else
+            throw new IllegalArgumentException("Worker not found " +workerId.toString());
+
     }
 }
