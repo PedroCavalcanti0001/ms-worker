385ef483a92d481dd2b2ba45afe429d12b6807f3
pedro <pedroeugeniocavalcanti@gmail.com>
2022-08-26 15:32:32 -0300

    OpenFeign adicionado no hr-payroll

--- hr-payroll/src/main/java/me/pedroeugenio/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/me/pedroeugenio/hrpayroll/services/PaymentService.java
@@ -2,33 +2,19 @@ package me.pedroeugenio.hrpayroll.services;
 
 import me.pedroeugenio.hrpayroll.entities.Payment;
 import me.pedroeugenio.hrpayroll.entities.Worker;
+import me.pedroeugenio.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayments(Long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-        Optional<Worker> workerOptional = Optional.ofNullable(restTemplate.getForObject(workerHost + "/workers/{id}",
-                Worker.class, uriVariables));
-        if (workerOptional.isPresent())
-            return new Payment(workerOptional.get().getName(), workerOptional.get().getDailyIncome(), 10);
-        else
-            throw new IllegalArgumentException("Worker not found " +workerId.toString());
+        Worker workerOptional = workerFeignClient.findById(workerId).getBody();
+        return new Payment(workerOptional.getName(), workerOptional.getDailyIncome(), 10);
 
     }
 }
