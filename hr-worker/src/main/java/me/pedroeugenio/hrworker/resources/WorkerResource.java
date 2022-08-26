ae2fa5452a400b280972e0b71a6558d8e548ae70
pedro <pedroeugeniocavalcanti@gmail.com>
2022-08-26 14:09:05 -0300

    Resource para endpoint de workers criado

---
+++ hr-worker/src/main/java/me/pedroeugenio/hrworker/resources/WorkerResource.java
@@ -0,0 +1,35 @@
+package me.pedroeugenio.hrworker.resources;
+
+import me.pedroeugenio.hrworker.entities.Worker;
+import me.pedroeugenio.hrworker.repositories.WorkerRepository;
+import me.pedroeugenio.hrworker.services.WorkerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerService workerService;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll(){
+        List<Worker> list = workerService.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Optional<Worker> optionalWorker = workerService.findById(id);
+        return optionalWorker.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+}
